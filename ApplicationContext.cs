5d2c7aa2d413f2fefeee84cb995971799e44c18c
HalimHamidov <halimjonik@gmail.com>
2020-11-16 22:02:23 +0300

    feat: CRUD ops https://metanit.com/sharp/entityframeworkcore/2.1.php

---
+++ ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelloApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
