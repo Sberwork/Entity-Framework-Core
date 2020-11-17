152e7b763158e31a00a1ed06869093a7428dab2a
Halim <46631802+HalimHamidov@users.noreply.github.com>
2020-11-17 18:58:04 +0300

    Update MyLoggerProvider.cs
    fix: MyLoggerProvider

--- HelloApp/MyLoggerProvider.cs
+++ HelloApp/MyLoggerProvider.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.Extensions.Logging;
+using System;
+using Microsoft.Extensions.Logging;
+using System.IO;
 
 namespace HelloApp
 {
-    internal class MyLoggerProvider : ILoggerProvider
+    public class MyLoggerProvider : ILoggerProvider
     {
         public ILogger CreateLogger(string categoryName)
         {
-            throw new System.NotImplementedException();
+            return new MyLogger();
         }
 
-        public void Dispose()
+        public void Dispose() { }
+
+        private class MyLogger : ILogger
         {
-            throw new System.NotImplementedException();
+            public IDisposable BeginScope<TState>(TState state)
+            {
+                return null;
+            }
+
+            public bool IsEnabled(LogLevel logLevel)
+            {
+                return true;
+            }
+
+            public void Log<TState>(LogLevel logLevel, EventId eventId,
+                TState state, Exception exception, Func<TState, Exception, string> formatter)
+            {
+                File.AppendAllText("log.txt", formatter(state, exception));
+                Console.WriteLine(formatter(state, exception));
+            }
         }
     }
 }
