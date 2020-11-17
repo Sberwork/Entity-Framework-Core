57572793cc1cdfe4089f91af4af7948ff0bfb905
HalimHamidov <halimjonik@gmail.com>
2020-11-17 17:36:44 +0300

    init logger

---
+++ v5/HelloApp/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelloApp
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+            optionsBuilder.LogTo(System.Console.WriteLine);
+        }
+    }
+}
---
+++ v5/HelloApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
+namespace HelloApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
+                User user1 = new User { Name = "Tom", Age = 33 };
+                User user2 = new User { Name = "Alice", Age = 26 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+            Console.Read();
+        }
+    }
+}
