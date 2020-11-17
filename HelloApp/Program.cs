1ff48f27e010ce16b9737ece81838c3e11bf0d75
HalimHamidov <halimjonik@gmail.com>
2020-11-17 18:12:17 +0300

    feat: add FileLogger

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
 using System.Linq;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using System.IO;
+using Microsoft.Extensions.Logging;
 
 namespace HelloApp
 {
@@ -10,36 +9,18 @@ namespace HelloApp
     {
         public static void Main(string[] args)
         {
-            var builder = new ConfigurationBuilder();
-            // установка пути к текущему каталогу
-            builder.SetBasePath(Directory.GetCurrentDirectory());
-            // получаем конфигурацию из файла appsettings.json
-            builder.AddJsonFile("appsettings.json");
-            // создаем конфигурацию
-            var config = builder.Build();
-            // получаем строку подключения
-            string connectionString = config.GetConnectionString("DefaultConnection");
-
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
-            var options = optionsBuilder
-                .UseSqlServer(connectionString)
-                .Options;
-
-            using (ApplicationContext db = new ApplicationContext(options))
+            using (ApplicationContext db = new ApplicationContext())
             {
-                // создаем два объекта User
+                db.GetService<ILoggerFactory>().AddProvider(new MyLoggerProvider());
                 User user1 = new User { Name = "Tom", Age = 33 };
                 User user2 = new User { Name = "Alice", Age = 26 };
-                 
-                // добавляем их в бд
+
                 db.Users.Add(user1);
                 db.Users.Add(user2);
                 db.SaveChanges();
-                Console.WriteLine("Объекты успешно сохранены");
-
-                // получаем объекты из бд и выводим на консоль
 
                 var users = db.Users.ToList();
+                Console.WriteLine("Данные после добавления:");
                 foreach (User u in users)
                 {
                     Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
---
+++ HelloApp/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace HelloApp
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+            var logger = loggerFactory.CreateLogger("FileLogger");
+
+            app.Run(async (context) =>
+            {
+                logger.LogInformation("Processing request {0}", context.Request.Path);
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
